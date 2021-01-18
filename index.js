a61efcbf43741a07a620a2b45f8a459d95cf0e85
Duc Nguyen <nahz3112@gmail.com>
2021-01-18 14:55:33 -0500

    Added chat application

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-const {joinUser, removeUser} = require('./users');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
