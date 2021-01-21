1099b5ea361a3ac175cc4e1c52db7f5996bbcc5c
Duc Nguyen <nahz3112@gmail.com>
2021-01-20 20:32:15 -0500

    Added mongo db connection

--- ai-duck/main.py
+++ ai-duck/main.py
@@ -15,11 +15,11 @@ stemmer = LancasterStemmer()
 with open('./data/intents.json') as file:
     data = json.load(file)
 
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    print("No old data found")
+training = []
+output = []
+
+
+def retrain_model():
     words = []
     labels = []
     docs_x = []
@@ -43,9 +43,6 @@ except:
     # Creating bag of words in any pattern
     # I.e ["a", "little","zebra",...] => [0,0,0,...]
 
-    training = []
-    output = []
-
     out_empty = [0 for _ in range(len(labels))]
 
     for x,doc in enumerate(docs_x):
@@ -66,8 +63,19 @@ except:
     training = numpy.array(training)
     output = numpy.array(output)
 
-    with open("data.pickle", "wb") as f:
+    with open("./data/data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output),f)
+
+
+
+# Main function
+
+try:
+    with open("./data/data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    retrain_model()
+
 
 ops.reset_default_graph()
 # Start with the length of input data
@@ -81,11 +89,8 @@ net = tflearn.regression(net)
 # DNN - type of neural network
 model = tflearn.DNN(net)
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
-    model.save("model.tflearn")
+model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
+model.save("./data/model.tflearn")
 
 def bag_of_words(s,words):
     bag = [0 for _ in range(len(words))]
@@ -107,9 +112,15 @@ def chat():
             break
 
         #Giving out probability
-        results = model.predict([bag_of_words(inp, words)])
+        results = model.predict([bag_of_words(inp, words)])[0]
         results_index = numpy.argmax(results)
         tag = labels[results_index]
-        print(tag)
 
+        if results[results_index] > 0.85:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg["responses"]
+            print(random.choice(responses))
+        else:
+            print("Wdym?")
 chat()
